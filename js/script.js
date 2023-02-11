e01bff37ce7527d5d9995787e9b20cd0d2816ea6
cipher <bhushansontakke7410@gmail.com>
2023-02-11 09:54:40 +0530

    My Portfolio

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// === for-Offcanvas ===
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "257px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+  
+// === for current year ===
+  
+const currentYear = document.getElementById('fullYear');
+const year = new Date();
+currentYear.innerHTML += `Copyright &copy${year.getFullYear()} Bhushan Sontakke.`;
+  
+
+// === for cursor-Pointer ===  
+
+let cursor = document.querySelector('.cursor');
+let cursor2 = document.querySelector('.cursor2');
+
+document.addEventListener('mousemove',function(e){
+    cursor.style.cssText = cursor2.style.cssText = 'left: '+e.clientX+'px; top:  '+e.clientY +'px;';
+});    
+
+
+
+
